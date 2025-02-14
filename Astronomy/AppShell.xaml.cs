bfe84643d90ee930bf6c9942f8206d1b72d8f9e7
Samir Shyamdasani Sadarangani <62108338+samirshyam@users.noreply.github.com>
2025-02-14 17:46:48 +0100

    Proyecto 5, commit 2: Integracion de diferentes maneras de navegar entre paginas; flyout, tabs y routing. Ultimo Commit.

--- Astronomy/AppShell.xaml.cs
+++ Astronomy/AppShell.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace Astronomy;
+﻿using Astronomy.Pages;
+
+namespace Astronomy;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+
+        Routing.RegisterRoute("astronomicalbodydetails", typeof(AstronomicalBodyPage));
     }
 }
